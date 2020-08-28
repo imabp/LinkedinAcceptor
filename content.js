42aec4d1be40a9c21f22925d8e4129df31d4d4f8
Abir <abir.pal899@gmail.com>
2020-08-29 03:11:46 +0530

    Updated Manifest and ContentJS

---
+++ content.js
@@ -0,0 +1,5 @@
+let accepts=document.getElementsByClassName('invitation-card__action-btn artdeco-button artdeco-button--2 artdeco-button--secondary ember-view')
+for(let i=0;i<accepts.length;i++)
+{
+    accepts[i].click();
+}
